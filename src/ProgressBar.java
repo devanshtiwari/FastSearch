7bd5f0f48187debcbd9ded9be959b05d4b29a0ec
Devansh Tiwari <devanshtiwari@iiitdmj.ac.in>
2016-08-17 11:28:39 +0530

    Documentation Update

--- src/ProgressBar.java
+++ src/ProgressBar.java
@@ -1,13 +1,19 @@
 /**
- * Created by devanshtiwari on 11-Aug-16.
+ * <h1>ProgressBar</h1>
+ * <p>A Class that will help to implement the Progess Bar</p>
+ * @author Devansh Tiwari
+ * @version 1.0
+ * @since 2016-08-11
  */
 
 public class ProgressBar extends Thread {
     String anim= "|/-\\";
     static boolean showProgress;
+
     public static void signal(){
         showProgress=false;
     }
+
     public void run() {
         int x = 0;
         showProgress=true;
